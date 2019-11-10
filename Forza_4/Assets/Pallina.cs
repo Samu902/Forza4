9b7e9330ae069e4a14d1fe636e7156320f1a0e36
Samu902 <samuelemarrani@gmail.com>
2019-11-10 17:07:24 +0100

    Bottoni con palline

---
+++ Forza_4/Assets/Pallina.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pallina : MonoBehaviour
+{
+    public GameObject pallinaRossa;
+    public GameObject pallinaGialla;
+    public float[] x;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GeneraPallina(int index)
+    {
+        GameObject g = Random.value >= 0.5f ? pallinaGialla : pallinaRossa;
+        Instantiate(g, new Vector3(x[index], 4.5f, 0), Quaternion.identity);
+    }
+}
